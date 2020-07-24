5c88dcaddd8e1ce2b6ad7a42abfce66304a1687a
Leidysalda <leidysalda1@gmail.com>
2020-07-24 20:59:55 +0000

    [Fix]: Betty style

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,28 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - function that measures the balance factor of a
- * binary tree
- *
- * @tree: tree
- *
- * Return: Always 0(Success)
- **/
-
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	size_t balanceT = NULL;
-
-	if (tree == NULL)
-		return (0);
-
-	balanceT = binary_tree_height(tree->left) -
-		binary_tree_height(tree->right);
-
-	return (balanceT);
-}
-
-/**
  * binary_tree_height - function that measures the height of a binary tree
  * @tree: tree
  *
@@ -34,17 +12,49 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t count_left = 0;
 	size_t count_right = 0;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        else if (tree->left == NULL && tree->right == NULL)
-                return (0);
+	if (tree->left)
+		count_left = 1 + binary_tree_height(tree->left);
 
-       count_left = binary_tree_height(tree->left);
-        count_right = binary_tree_height(tree->right);
+	if (tree->right)
+		count_right = 1 + binary_tree_height(tree->right);
 
-        if (count_left > count_right)
-                return (count_left + 1);
-        else
-                return (count_right + 1);
+
+	if (count_left > count_right)
+		return (count_left);
+	else
+		return (count_right);
+
+	return(0);
+}
+
+
+/**
+ * binary_tree_balance - function that measures the balance factor of a
+ * binary tree
+ *
+ * @tree: tree
+ *
+ * Return: Always 0(Success)
+ **/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t balanceT_1 = NULL;
+	size_t balanceT_2 = NULL;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		balanceT_1 = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		balanceT_1 = 1 + binary_tree_height(tree->right);
+
+	return (balanceT_1 - balanceT_2);
+
+	return(0);
 }
