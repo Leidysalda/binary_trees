26b132d3827a918afe4a36e9975feed16c15ee7d
jgarcia15121982 <johfregarcia@gmail.com>
2020-07-23 19:56:20 -0500

    [FIX] Function in order

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -9,14 +9,10 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL && func != NULL)
-	{
-		if (tree->left)
-			binary_tree_inorder(tree->left, func);
+	if (tree == NULL || func == NULL)
+		return;
 
-		func(tree->n);
-		
-		if (tree->right)
-			binary_tree_inorder(tree->right, func);
-	}
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
